598184598ae19ee935105367c3ae7c3a7ad4ba95
张明炜 <buddyzhang@rdchina.net>
2022-10-08 10:32:56 +0800

    创建出国英语项目

---
+++ GoAbroadEnglish/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GoAbroadEnglish
+//
+//  Created by zhangmingwei on 2022/10/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoAbroadEnglish/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  GoAbroadEnglish
+//
+//  Created by zhangmingwei on 2022/10/8.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
